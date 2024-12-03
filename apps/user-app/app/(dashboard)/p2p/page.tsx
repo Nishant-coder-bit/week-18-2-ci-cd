391629b7a89d6726c8bc64225a69ae6ef126410d
Nishant Raj <nishantanindian@gmail.com>
2024-12-03 21:28:40 +0530

    pushing to dockerhub

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -3,5 +3,6 @@ import { SendCard } from "../../../components/SendCard";
 export default function() {
     return <div className="w-full"> 
         <SendCard />
+        <h1>Pushing to dockerhub(cd)</h1>
     </div>
 }
