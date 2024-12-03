e7db1940edcc5ff1fa5f935fd66af981eece9098
Nishant Raj <nishantanindian@gmail.com>
2024-12-03 22:48:07 +0530

    ec2

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -3,6 +3,7 @@ import { SendCard } from "../../../components/SendCard";
 export default function() {
     return <div className="w-full"> 
         <SendCard />
+        <h2>deploying to ec2 instance</h2>
         <h1>Pushing to dockerhub(cd)</h1>
     </div>
 }
